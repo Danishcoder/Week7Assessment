514f796a563cd8a36069089409ecff13d1318e6f
DanishCoder <Danishim66@gmail.com>
2022-07-14 14:25:51 -0400

    FinalCommit

--- runtime.js
+++ runtime.js
@@ -64,3 +64,34 @@ extraLargeArray: getSizedArray(100000),
     console.log("append", resultsAppend.preciseWords);
 }
 
+
+
+// Results Table
+
+// Results for the tinyArray
+// insert 30.2 μs
+// append 62.6 μs
+
+// Results for the smallArray
+// insert 11.5 μs
+// append 9.3 μs
+
+// Results for the mediumArray
+// insert 107.8 μs
+// append 31.7 μs
+
+// Results for the largeArray
+// insert 6.768 ms
+// append 286.7 μs
+
+// Results for the extraLargeArray
+// insert 609.8499 ms
+// append 2.6312 ms
+
+
+//paragraph explanation
+/*
+The insert function performs almost as well as the append function on smaller arrays but the difference between speed is notable when the array
+gets larger, with the append function being orders of magnitude faster. I can tell that the append function scales better because I can see the that
+the difference between the speed of insert and append starts to get much larger as the quantity of the array gets larger.
+*/
